991f74cc0a7d8f99217a74885a66ac0fa4a3ee3d
abazdev34 <abaztoktorbaev89@gmail.com>
2024-04-25 20:31:42 +0300

    first commit

---
+++ to-do-list.js
@@ -0,0 +1,50 @@
+// crud -create read{rename} update delete
+
+
+// to do list
+
+
+
+
+const ul=document.querySelector("ul")
+const btn=document.querySelector("button")
+const input=document.querySelector("input")
+btn.addEventListener("click",(e)=>{
+    if(input.value === ""){
+        return false
+    }
+    let newText = `<li class="list-group-item d-flex justify-content-between 
+    align-items-center">
+
+    ${input.value} 
+    <BUtton class="del-btn btn btn-success">delete</button>
+    </li>`
+    ul.innerHTML += newText
+    input.value=""
+
+})
+ul.addEventListener("click",(e)=>{
+console.log(e.target.classList.contains("del-btn"));
+if(e.target.classList.contains("del-btn")){
+    e.target.parentNode.remove()
+}
+})
+input.addEventListener("keydown",(e)=>{
+   
+    if(e.key==="Enter"){
+     
+        if(input.value===""){
+            return false
+        }
+        
+        let newText = `<li class="list-group-item d-flex justify-content-between 
+        align-items-center">
+    
+        ${input.value} 
+        <BUtton class="del-btn btn btn-success">delete</BUtton>
+        </li>`
+        ul.innerHTML += newText
+        
+        input.value="" 
+    }
+})
